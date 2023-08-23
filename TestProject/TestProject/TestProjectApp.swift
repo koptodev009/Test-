56d9b1ce1b5a73f544d641af016e75ea6565e670
Sharvari Chavan <koptotech009@gmail.com>
2023-08-23 13:21:51 +0530

    First Project

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Kopto Tech on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
